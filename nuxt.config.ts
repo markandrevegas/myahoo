1425bc2733249c410484b7e082fcd4015a77693a
markandrevegas <markandrevega@me.com>
2025-11-10 21:07:31 +0100

    favicons. worked on build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,12 +15,25 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700&display=swap'
-        }
+        },
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-title', content: 'myahoo' }
       ]
     }
   },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  vite: {
+    server: {
+      allowedHosts: ['myahoo.local']
+    }
+	},
   css: ["~/assets/css/tailwind.css"],
   modules: [
     "@nuxtjs/tailwindcss",
@@ -35,5 +48,11 @@ export default defineNuxtConfig({
       openWeatherApiKey: process.env.OPENWEATHER_API_KEY,
       unsplashAccessKey: process.env.UNSPLASH_ACCESS_KEY
     }
-  }
+  },
+  nitro: {
+		preset: "node-server",
+		externals: {
+			inline: ["ofetch"]
+		}
+	}
 })
